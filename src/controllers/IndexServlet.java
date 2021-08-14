394a7a80e3ab9afe2e685a3d967c05e17ea6a01d
takanori.teshigahara <takanori_teshigahara@r.recruit.co.jp>
2021-08-14 17:51:36 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,8 +36,12 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Tasklist> tasklist = em.createNamedQuery("getAllTasks", Tasklist.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasklist.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasklist", tasklist);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklist/index.jsp");
+        rd.forward(request, response);
     }
 }
