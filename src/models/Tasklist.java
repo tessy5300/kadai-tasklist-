9fea1d2eb023fb03ed02dee14000ce5bf4222b41
takanori.teshigahara <takanori_teshigahara@r.recruit.co.jp>
2021-08-14 17:21:38 +0900

    geAllTasksを追加

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -7,10 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "taskilist")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Tasklist AS m ORDER BY m.id DESC"
+    )
+})
+
+@Table(name = "tasklist")
 public class Tasklist {
     @Id
     @Column(name = "id")
